339434b1339d7bf02056380e49f6d32ade6357d3
bruderj15 <julian.bruder@outlook.com>
2025-04-06 19:57:06 +0200

    main: format

--- src/item/mod.rs
+++ src/item/mod.rs
@@ -1,6 +1,6 @@
 pub mod item {
-    pub(crate) mod itemstate;
-    pub(crate) mod itemtype;
     pub(crate) mod currency;
     pub(crate) mod item;
+    pub(crate) mod itemstate;
+    pub(crate) mod itemtype;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
 mod item;
 mod sources;
 
-use std::error::Error;
-use reqwest::Client;
 use crate::item::item::currency::Currency::{EUR, GBP};
-use crate::sources::sources::militariamart::Militariamart;
 use crate::sources::sources::Source;
+use crate::sources::sources::militariamart::Militariamart;
+use reqwest::Client;
+use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let century20militaria = Militariamart::new("https://20thcenturymilitaria.com/".to_string(), GBP);
-    let antiquitiesofthereich = Militariamart::new("https://antiquitiesofthereich.com/".to_string(), GBP);
+    let century20militaria =
+        Militariamart::new("https://20thcenturymilitaria.com/".to_string(), GBP);
+    let antiquitiesofthereich =
+        Militariamart::new("https://antiquitiesofthereich.com/".to_string(), GBP);
     let hollandpatch = Militariamart::new("https://hollandpatch.com/".to_string(), EUR);
     let khakicolonel = Militariamart::new("https://khakicolonel.com/".to_string(), GBP);
-    
+
     let client = Client::new();
     let items = hollandpatch.gather(&client).await?;
 
     let size = items.len();
-    
+
     for item in items {
         println!("{}\n", serde_json::to_string_pretty(&item).unwrap())
     }
-    
+
     println!("Size: {}", size);
-    
+
     Ok(())
 }
