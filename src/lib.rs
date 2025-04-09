2fba8fba7d416d7f806d2037bb656d5b03e3b869
bruderj15 <julian.bruder@outlook.com>
2025-04-09 20:11:19 +0200

    main: make Error require `Send + Sync`

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,13 @@ pub mod scrape {
             &self,
             page_num: i16,
             client: &Client,
-        ) -> Result<Vec<ItemDiff>, Box<dyn Error>>;
+        ) -> Result<Vec<ItemDiff>, Box<dyn Error + Send + Sync>>;
 
         async fn gather(
             &self,
             client: &Client,
             sleep_between_pages_millis: Option<u64>,
-        ) -> Result<Vec<ItemDiff>, Box<dyn Error>> {
+        ) -> Result<Vec<ItemDiff>, Box<dyn Error + Send + Sync>> {
             let mut all_items = Vec::new();
             let mut i: i16 = 1;
             loop {
