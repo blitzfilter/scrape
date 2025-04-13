f79b3e61aeddf74adb264264e26f9f6a99a6e52a
bruderj15 <julian.bruder@outlook.com>
2025-04-13 15:15:47 +0200

    main: use lib item-read

--- src/hash_comparison.rs
+++ src/hash_comparison.rs
@@ -1,16 +1,8 @@
 use item_core::item_data::ItemData;
-use item_core::item_hash::{ItemEventHash, ItemHash};
+use item_core::item_hash::ItemHash;
 use std::collections::HashMap;
 
-// assumes hashes to be sorted by latest event_id
-pub fn drop_irrelevant_diffs(diffs: &mut Vec<ItemData>, hashes: &Vec<ItemEventHash>) {
-    let mut item_id_hash_map: HashMap<&str, &str> = HashMap::new();
-    for item_event_hash in hashes {
-        item_id_hash_map
-            .entry(item_event_hash.get_item_id())
-            .or_insert(&item_event_hash.hash);
-    }
-
+pub fn drop_unchanged_diffs(diffs: &mut Vec<ItemData>, item_id_hash_map: &HashMap<String, String>) {
     diffs.retain(|diff| {
         let old_hash = item_id_hash_map.get(diff.item_id.as_str());
         if old_hash.is_none() {
@@ -24,21 +16,22 @@ pub fn drop_irrelevant_diffs(diffs: &mut Vec<ItemData>, hashes: &Vec<ItemEventHa
 
 #[cfg(test)]
 mod tests {
-    use crate::hash_comparison::drop_irrelevant_diffs;
+    use crate::hash_comparison::drop_unchanged_diffs;
     use item_core::item_data::ItemData;
-    use item_core::item_hash::{ItemEventHash, hash_item_details};
-    use item_core::item_state::ItemState::{AVAILABLE, LISTED, RESERVED, SOLD};
+    use item_core::item_hash::hash_item_details;
+    use item_core::item_state::ItemState::{AVAILABLE, SOLD};
     use item_core::price::Currency::EUR;
     use item_core::price::Price;
+    use std::collections::HashMap;
 
     fn make_items_diffs() -> Vec<ItemData> {
         vec![
-            ItemData::new("item#foo#bar".to_string())
-                .item_state(SOLD)
+            ItemData::new("foo#bar".to_string())
+                .state(SOLD)
                 .url("https://foo.com/item=bar".to_string())
                 .clone(),
-            ItemData::new("item#foo#baz".to_string())
-                .item_state(AVAILABLE)
+            ItemData::new("foo#baz".to_string())
+                .state(AVAILABLE)
                 .price(Price::new(EUR, 42f32))
                 .url("https://foo.com/item=baz".to_string())
                 .clone(),
@@ -46,23 +39,15 @@ mod tests {
     }
 
     #[test]
-    fn should_not_drop_any_diffs_when_all_hashes_differ() {
+    fn should_not_drop_any_diffs_when_all_latest_hashes_differ() {
         let expected: Vec<ItemData> = make_items_diffs();
         let mut actual: Vec<ItemData> = expected.clone();
-        let hashes: Vec<ItemEventHash> = vec![
-            ItemEventHash {
-                event_id: "item#foo#bar#2025-01-02T12:00:00.001+01:00".to_string(),
-                source_id: "item#foo".to_string(),
-                hash: hash_item_details(Some(AVAILABLE), Some(42f32)),
-            },
-            ItemEventHash {
-                event_id: "item#foo#bar#2025-01-01T12:00:00.001+01:00".to_string(),
-                source_id: "item#foo".to_string(),
-                hash: hash_item_details(Some(LISTED), None),
-            },
-        ];
+        let hashes = HashMap::from([(
+            "foo#bar".to_string(),
+            hash_item_details(Some(AVAILABLE), Some(42f32)),
+        )]);
 
-        drop_irrelevant_diffs(&mut actual, &hashes);
+        drop_unchanged_diffs(&mut actual, &hashes);
 
         assert_eq!(expected, actual);
     }
@@ -70,25 +55,15 @@ mod tests {
     #[test]
     fn should_drop_all_diffs_when_all_latest_hashes_match() {
         let mut actual: Vec<ItemData> = make_items_diffs();
-        let hashes: Vec<ItemEventHash> = vec![
-            ItemEventHash {
-                event_id: "item#foo#bar#2025-02-01T12:00:00.001+01:00".to_string(),
-                source_id: "item#foo".to_string(),
-                hash: hash_item_details(Some(SOLD), None),
-            },
-            ItemEventHash {
-                event_id: "item#foo#bar#2025-01-01T12:00:00.001+01:00".to_string(),
-                source_id: "item#foo".to_string(),
-                hash: hash_item_details(Some(RESERVED), None),
-            },
-            ItemEventHash {
-                event_id: "item#foo#baz#2025-01-01T12:00:00.001+01:00".to_string(),
-                source_id: "item#foo".to_string(),
-                hash: hash_item_details(Some(AVAILABLE), Some(42f32)),
-            },
-        ];
+        let hashes = HashMap::from([
+            ("foo#bar".to_string(), hash_item_details(Some(SOLD), None)),
+            (
+                "foo#baz".to_string(),
+                hash_item_details(Some(AVAILABLE), Some(42f32)),
+            ),
+        ]);
 
-        drop_irrelevant_diffs(&mut actual, &hashes);
+        drop_unchanged_diffs(&mut actual, &hashes);
 
         assert!(actual.is_empty());
     }
@@ -96,29 +71,22 @@ mod tests {
     #[test]
     fn should_retain_only_actual_diffs() {
         let mut actual: Vec<ItemData> = make_items_diffs();
-        let hashes: Vec<ItemEventHash> = vec![
-            ItemEventHash {
-                event_id: "item#foo#bar#2025-02-01T12:00:00.001+01:00".to_string(),
-                source_id: "item#foo".to_string(),
-                hash: hash_item_details(Some(RESERVED), None),
-            },
-            ItemEventHash {
-                event_id: "item#foo#bar#2025-01-01T12:00:00.001+01:00".to_string(),
-                source_id: "item#foo".to_string(),
-                hash: hash_item_details(Some(LISTED), None),
-            },
-            ItemEventHash {
-                event_id: "item#foo#baz#2025-01-01T12:00:00.001+01:00".to_string(),
-                source_id: "item#foo".to_string(),
-                hash: hash_item_details(Some(AVAILABLE), Some(42f32)),
-            },
-        ];
+        let hashes = HashMap::from([
+            (
+                "foo#bar".to_string(),
+                hash_item_details(Some(AVAILABLE), Some(42f32)),
+            ),
+            (
+                "foo#baz".to_string(),
+                hash_item_details(Some(AVAILABLE), Some(42f32)),
+            ),
+        ]);
 
-        drop_irrelevant_diffs(&mut actual, &hashes);
+        drop_unchanged_diffs(&mut actual, &hashes);
 
         let expected = vec![
-            ItemData::new("item#foo#bar".to_string())
-                .item_state(SOLD)
+            ItemData::new("foo#bar".to_string())
+                .state(SOLD)
                 .url("https://foo.com/item=bar".to_string())
                 .clone(),
         ];
