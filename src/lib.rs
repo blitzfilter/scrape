f68e64023a01d62b7b2cb6d4795c2d9089356e32
bruderj15 <julian.bruder@outlook.com>
2025-05-01 10:30:11 +0200

    main: test entire scrape to persist flow

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,60 @@ pub mod hash_comparison;
 pub mod scraper;
 pub mod scraper_config;
 
+use crate::hash_comparison::drop_unchanged_diffs;
+use crate::scraper::Scraper;
+use crate::scraper_config::ScraperConfig;
+use aws_sdk_sqs::types::SendMessageBatchRequestEntry;
+use futures::StreamExt;
 pub use item_core;
+use item_read::item_hash::get_latest_item_event_hash_map_by_source_id;
+use uuid::Uuid;
+
+pub const MAX_SQS_BATCH_SIZE: usize = 10;
+
+pub async fn scrape_and_push(
+    scraper: &impl Scraper,
+    scraper_config: &ScraperConfig,
+    reqwest_client: &reqwest::Client,
+    sqs_client: &aws_sdk_sqs::Client,
+    dynamodb_client: &aws_sdk_dynamodb::Client,
+) {
+    let source_id = format!("source#{}", scraper_config.base_url);
+    let item_hashes_map = get_latest_item_event_hash_map_by_source_id(&source_id, dynamodb_client)
+        .await
+        .expect("TODO: Shouldn't fail retrieving latest item hashes");
+    scraper
+        .scrape(reqwest_client, scraper_config.sleep_between_pages_millis)
+        .chunks(MAX_SQS_BATCH_SIZE)
+        .for_each_concurrent(5, |diff_results| async {
+            let mut diffs = diff_results
+                .into_iter()
+                .filter_map(|diff_result| diff_result.ok())
+                .collect::<Vec<_>>();
+
+            drop_unchanged_diffs(&mut diffs, &item_hashes_map);
+
+            let msg_entries = diffs
+                .into_iter()
+                .map(|diff| {
+                    SendMessageBatchRequestEntry::builder()
+                        .message_body(
+                            serde_json::to_string(&diff)
+                                .expect("TODO: shouldn't fail converting to json"),
+                        )
+                        .id(Uuid::new_v4().to_string())
+                        .build()
+                        .expect("shouldn't fail because 'id' and 'message_body' have been set")
+                })
+                .collect::<Vec<_>>();
+
+            sqs_client
+                .send_message_batch()
+                .queue_url("http://sqs.eu-central-1.localhost.localstack.cloud:4566/000000000000/write_lambda_queue")
+                .set_entries(Some(msg_entries))
+                .send()
+                .await
+                .expect("TODO: handle sqs error");
+        })
+        .await;
+}
---
+++ tests/integration.rs
@@ -0,0 +1,58 @@
+use async_trait::async_trait;
+use item_core::item_data::ItemData;
+use item_read::item_hash::get_item_event_hashes_by_source_id;
+use scrape::scrape_and_push;
+use scrape::scraper::{ScrapeError, Scraper};
+use scrape::scraper_config::ScraperConfig;
+use std::time::Duration;
+use test_api::generator::Generator;
+use test_api::localstack::{get_dynamodb_client, get_sqs_client};
+use test_api::test_api_macros::blitzfilter_data_ingestion_test;
+use tokio::time::sleep;
+
+struct TestScraper {}
+
+#[async_trait]
+impl Scraper for TestScraper {
+    async fn scrape_page(
+        &self,
+        page_num: i16,
+        _: &reqwest::Client,
+    ) -> Result<Vec<ItemData>, ScrapeError> {
+        match page_num {
+            1 => Ok(vec![ItemData::generate().source_id("http://foo.bar".to_string()).to_owned()]),
+            _ => Ok(vec![]),
+        }
+    }
+}
+
+#[blitzfilter_data_ingestion_test]
+async fn should_scrape_push_sqs_trigger_lambda_insert_dynamodb() {
+    let scraper = TestScraper {};
+    let scraper_config = ScraperConfig {
+        sleep_between_pages_millis: None,
+        base_url: "http://foo.bar".to_string(),
+    };
+    let reqwest_client = reqwest::Client::new();
+    let sqs_client = get_sqs_client().await;
+    let dynamodb_client = get_dynamodb_client().await;
+
+    scrape_and_push(
+        &scraper,
+        &scraper_config,
+        &reqwest_client,
+        sqs_client,
+        dynamodb_client,
+    )
+    .await;
+
+    // Wait for SQS and Lambda to work...
+    sleep(Duration::from_secs(15)).await;
+
+    let read_res =
+        get_item_event_hashes_by_source_id("http://foo.bar", false, dynamodb_client).await;
+    assert!(read_res.is_ok());
+    
+    let read = read_res.unwrap();
+    assert_eq!(read.len(), 1);
+}
